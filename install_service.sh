1d206701c91ec655b03c59adf5c3344dccde671a
heidenbloed <git@heidenblut.eu>
2021-01-06 22:40:58 +0100

    Run service as root. + Fix service installation. + Fix requirements.

--- install_service.sh
+++ install_service.sh
@@ -4,14 +4,13 @@ if [ ! -f venv/bin/activate ]; then
   echo "Install virtual python environment"
   python3.8 -m venv venv
 fi
-source venv/bin/activate
+. venv/bin/activate
 echo "Install python requirements"
 pip install -r ./requirements.txt
 echo "Copy service"
 sudo cp nrf24_manager.service /etc/systemd/system/
-echo "Adapt service for current user"
-sudo sed -i "s@USERNAME@$USER@" /etc/systemd/system/nrf24_manager.service
-sudo sed -i "s@HOMEDIR@$HOME@" /etc/systemd/system/nrf24_manager.service
+echo "Adapt service for current working dictionary"
+sudo sed -i "s@PWD@$PWD@" /etc/systemd/system/nrf24_manager.service
 echo "Enable service"
 sudo systemctl enable nrf24_manager.service
 echo "Start service"
